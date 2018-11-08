091922cd11a2d5276ee244444eeb2d6ee7791788
marthe.charpy <marthe.charpy@gmail.com>
2018-11-08 17:49:56 +0100

    création de message d'accueil et du menu

---
+++ DisplayGame.java
@@ -0,0 +1,16 @@
+public class DisplayGame{
+
+	public static String displayAccueil() {
+		
+		return "Bienvenue à la taverne des Gobelins Fringants!!! \nEtes-vous prêts à entamer un terrrrrrible duel de sorciers?";
+	}
+
+	public static String displayMenu() {
+		
+		return "1-Jouer \n2-Hall of Fame \n3-Règles du jeu";
+	}
+
+	
+
+
+}
---
+++ Game.java
@@ -0,0 +1,13 @@
+public class Game{
+		
+	public static void main (String[] args) {
+
+
+		System.out.println(DisplayGame.displayAccueil());
+
+		System.out.println(DisplayGame.displayMenu());
+
+
+	}
+
+}
