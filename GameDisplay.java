f9ed0024d0c2d01488c4f1da8edec08249c7e873
exonihilo <erwan.geneve@icloud.com>
2018-11-14 10:44:59 +0100

    Modification du comportement du menu: -Gestion des exceptions InputMisMatchException. -Gestion de l'entrée du choix menu.

---
+++ GameDisplay.java
@@ -0,0 +1,16 @@
+public class GameDisplay{
+
+	public static void welcomeDisplay() {
+		
+		System.out.println("Bienvenue à la taverne des Gobelins Fringants!!! \nEtes-vous prêts à entamer un terrrrrrible duel de sorciers?");
+	}
+
+	public static void menuDisplay() {
+		
+		System.out.println("1-Jouer \n2-Hall of Fame \n3-Règles du jeu");
+	}
+
+	
+
+
+}
