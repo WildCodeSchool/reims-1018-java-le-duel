fda8745a02a0fbe2e3350865db613e352f946374
exonihilo <erwan.geneve@icloud.com>
2018-11-15 10:54:40 +0100

    Modification du nom du fichier Sort.json en Spell.json Ajout d'un parser dans SpellInit

--- SpellInit.java
+++ SpellInit.java
@@ -11,6 +11,26 @@ import org.json.simple.parser.ParseException;
 // Execute : java -cp .:json-simple-1.1.1.jar WeatherParse
 
 public class SpellInit{
+	
+	private static String JSON_WEATHER_PATH = "weather.json";
 
+    public static void main(String[] args) {
+
+        FileReader jsonFile = null;
+        try {
+            // lecture du fichier json
+            jsonFile = new FileReader(JSON_WEATHER_PATH);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        
+        //TODO parser le fichier
+			JSONParser parser = new JSONParser();
+			Object jsonParsed = null;
+			try{
+				jsonParsed = parser.parse(jsonFile);
+			} catch (ParseException | IOException e) {
+				e.printStackTrace();
+}
 
 }
