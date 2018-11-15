6a4546d9d81a030867cdf9e28f6d3c98e4dbc4b3
exonihilo <erwan.geneve@icloud.com>
2018-11-15 10:47:28 +0100

    Création du fichier SpellInit.java Modification du fichier .gitignore: -retirer *.jar du fichier

---
+++ SpellInit.java
@@ -0,0 +1,16 @@
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+// Compile : javac -cp .:json-simple-1.1.1.jar WeatherParse.java
+// Execute : java -cp .:json-simple-1.1.1.jar WeatherParse
+
+public class SpellInit{
+
+
+}
