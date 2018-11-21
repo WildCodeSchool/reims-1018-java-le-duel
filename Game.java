4a2397fe3ad79816d38224cec83d9ade22ce1b02
marthe.charpy <marthe.charpy@gmail.com>
2018-11-21 16:24:39 +0100

    instanciation de Player

--- Game.java
+++ Game.java
@@ -32,30 +32,32 @@ public class Game{
 	
 	public static void party(){
 		var spellInit = new SpellInit();
-		String player1 = GameDisplay.playerDisplay();
-		String player2 = GameDisplay.playerDisplay();
+		String player1Name = GameDisplay.playerDisplay();
+		String player2Name = GameDisplay.playerDisplay();
+		Player player1 = new Player(player1Name);
+		Player player2 = new Player(player2Name);
 		int choice1 = GameDisplay.partyDisplay();
 		int choice2 = GameDisplay.partyDisplay();
 		String spellType1 = spellInit.getSpellType(choice1);
-		String spellType2= spellInit.getSpellType(choice2);
+		String spellType2 = spellInit.getSpellType(choice2);
 		String spellWeakness1 = spellInit.getSpellWeakness(choice1);
-		String spellWeakness2= spellInit.getSpellWeakness(choice2);
-		String result = Game.battle(spellType1, spellType2, spellWeakness1, spellWeakness2);
+		String spellWeakness2 = spellInit.getSpellWeakness(choice2);
+		String result = Game.battle(spellType1, spellType2, spellWeakness1, spellWeakness2, player1, player2);
 		System.out.println(result);
 	}
 
-	public static String battle(String spellType1, String spellType2, String spellWeakness1, String spellWeakness2){
+	public static String battle(String spellType1, String spellType2, String spellWeakness1, String spellWeakness2, Player player1, Player player2){
 		if (spellType1.equals(spellType2)){
 			return "Egalité!";
 		}
 		if (spellType1.equals(spellWeakness2)){
-			return "Joueur 1 remporte le duel";
+			return player1.getName() + " remporte le duel";
 		}
 		if (spellType2.equals(spellWeakness1)){
-			return "Joueur 2 remporte le duel!";
+			return player2.getName() + " remporte le duel!";
 		}
 		else{
-			return "BOOM! Les 2 joueurs sont à terre!";
+			return "BOOM! " + player1.getName() + " et " + player2.getName() + " sont à terre!";
 		}
 	}
 }
